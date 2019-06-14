3326f9f5515f42277766140676766157d447db3d
Kevin Arellano <Kevin.Arellano94@gmail.com>
2019-06-14 16:32:14 -0400

    .
    .

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const auth = require("./auth.json");
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
+client.on("ready", () => {
+  console.log("Connected as " + client.user.tag);
 });
 
-client.login(auth.token);
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+const bot_secret_token = "588790935266131990";
+
+client.login(bot_secret_token);
